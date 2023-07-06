e9f9fd54fd881472055933be79c6a5a576201f21
unknown <abhishekvyawhare7@gmail.com>
2023-07-06 12:52:38 +0530

    Java_8

--- src/programming/FP03BehaviourParameterization.java
+++ src/programming/FP03BehaviourParameterization.java
@@ -11,9 +11,9 @@ public class FP03BehaviourParameterization {
 		List<Integer> numbers = List.of(12, 9, 2, 13, 4, 6, 4, 12, 15);
 	
 		
-//		filterandprint(numbers, x->x%2==0);
+		filterandprint(numbers, x->x%2==0);
 		
-//		filterandprint(numbers,  x->x%2==1);
+		filterandprint(numbers,  x->x%2==1);
 		
 		filterandprint(numbers,  x->x%3==0);
 		//we are passing the alogorithim, hte strategy as a parameter. we are externalazing this strategy 
